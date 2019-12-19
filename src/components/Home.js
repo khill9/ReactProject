c0271f09f33defcfc7196d0cd8932066ee75140a
Kaylie Hill <kaylie.hill@regions.com>
2019-12-19 14:31:52 -0600

    added PI API call to redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PersonalInfo from './PersonalInfo';
 import ReactLoading from "react-loading";
 import './Loading.css';
+import fetchProductsAction from '../redux/reducer';
+import { fetchProducts } from '../redux/reducer';
+import {getProductsError, getProducts, getProductsPending} from '../redux/reducer';
+
 
 class Home extends Component {
 
@@ -10,12 +16,30 @@ class Home extends Component {
       super(props);
       this.state = {
         error: null,
-        isLoaded: false,
-        user: []
+        pending: false,
+        products: []
       };
+      this.shouldComponentRender = this.shouldComponentRender.bind(this);
     }
 
-    componentDidMount() {
+    componentWillMount() {
+      const {fetchProducts} = this.props;
+      fetchProducts();
+
+      // Hide Login Portion once authenticated
+      var loginForm = document.getElementById("loginForm");
+      loginForm.style.display = "none";
+    }
+
+  shouldComponentRender() {
+      const {pending} = this.props;
+      if(this.pending === false) return false;
+      // more tests
+      return true;
+  }
+
+    //componentDidMount() {
+      /*
         fetch("https://randomuser.me/api/")
           .then(res => res.json())
           .then(
@@ -34,24 +58,43 @@ class Home extends Component {
             });
           }
         )
-      // Hide Login Portion once authenticated
-      var loginForm = document.getElementById("loginForm");
-      loginForm.style.display = "none";
-    }
+        */
+       //this.props.fetchProducts();
+
+      
+    //}
 
     render() {
-        const { error, isLoaded, user } = this.state;
+        const { error, user } = this.state;
         if (error) {
           return <div class="alert alert-danger" role="alert">Error: {error.message}</div>;
-        } else if (!isLoaded) {
-          return <div class="loader"><ReactLoading type={"cylon"} color={"black"} /></div>;
+        } else if(this.props.products == undefined){
+          return <div class="loader"><ReactLoading type={"cylon"} color={"black"} /></div>
         } else {
           return (
-            <PersonalInfo  user={user}/>
+            <PersonalInfo  user={this.props.products}/>
           );
         }
       }
 }
 
+const mapStateToProps = (state) => ({
+  error: getProductsError(state),
+  products: getProducts(state),
+  pending: getProductsPending(state)
+})
 
-export default Home
+//const mapDispatchToProps = (dispatch) => bindActionCreators({
+  //fetchProducts: fetchProductsAction
+//}, dispatch)
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchProducts: (test) => dispatch(fetchProducts("test"))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home );
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,6 +2,10 @@ const SET_LOGIN_PENDING = 'SET_LOGIN_PENDING';
 const SET_LOGIN_SUCCESS = 'SET_LOGIN_SUCCESS';
 const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
 
+const FETCH_PRODUCTS_PENDING = 'FETCH_PRODUCTS_PENDING';
+const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+const FETCH_PRODUCTS_ERROR = 'FETCH_PRODUCTS_ERROR';
+
 export function login(email, password) {
   return dispatch => {
     dispatch(setLoginPending(true));
@@ -18,6 +22,59 @@ export function login(email, password) {
       }
     });
   }
+}
+
+export function fetchProducts(test) {
+  return dispatch => {
+      dispatch(fetchProductsPending());
+      fetch("https://randomuser.me/api/")
+          .then(result => result.json())
+          .then(
+            (result) => {
+              dispatch(fetchProductsSuccess(result.results[0]));
+              return result.results[0];
+            },
+           (error) => {
+              this.setState({
+                pending: false,
+              error
+            });
+            dispatch(fetchProductsError(error));
+          }
+        )
+
+
+
+
+      //fetch('https://randomuser.me/api/')
+      //.then(res => res.json())
+      //.then(res => {
+      //    if(res.error) {
+      //        throw(res.error);
+      //    }
+      //    dispatch(fetchProductsSuccess(res.result.results[0]));
+      //    //return res.result.results[0];
+      //})
+      //.catch(error => {
+      //    dispatch(fetchProductsError(error));
+      //})
+  }
+}
+
+function callLoginApi(email, password, callback) {
+  setTimeout(() => {
+    if (email === 'admin@example.com' && password === 'admin') {
+      return callback(null);
+    } 
+    if (email === 'kaylie.hill@regions.com' && password === 'password') {
+      return callback(null);
+    } 
+    if (email === 'khill@cgi.com' && password === 'pw') {
+      return callback(null);
+    } else {
+      return callback(new Error('Invalid email and password'));
+    }
+  }, 1000);
 }
 
 function setLoginPending(isLoginPending) {
@@ -41,26 +98,34 @@ function setLoginError(loginError) {
   }
 }
 
-function callLoginApi(email, password, callback) {
-  setTimeout(() => {
-    if (email === 'admin@example.com' && password === 'admin') {
-      return callback(null);
-    } 
-    if (email === 'kaylie.hill@regions.com' && password === 'password') {
-      return callback(null);
-    } 
-    if (email === 'khill@cgi.com' && password === 'pw') {
-      return callback(null);
-    } else {
-      return callback(new Error('Invalid email and password'));
-    }
-  }, 1000);
+function fetchProductsPending(pending) {
+  return {
+      type: FETCH_PRODUCTS_PENDING,
+      pending
+  }
+}
+
+function fetchProductsSuccess(products) {
+  return {
+      type: FETCH_PRODUCTS_SUCCESS,
+      products
+  }
+}
+
+function fetchProductsError(error) {
+  return {
+      type: FETCH_PRODUCTS_ERROR,
+      error
+  }
 }
 
 export default function reducer(state = {
   isLoginSuccess: false,
   isLoginPending: false,
-  loginError: null
+  loginError: null,
+  pending: false,
+  products: null,
+  error: null
 }, action) {
   switch (action.type) {
     case SET_LOGIN_PENDING:
@@ -78,7 +143,27 @@ export default function reducer(state = {
         loginError: action.loginError
       });
 
+    case FETCH_PRODUCTS_PENDING:
+      return Object.assign({}, state, {
+        pending: action.pending
+      });
+
+    case FETCH_PRODUCTS_SUCCESS:
+      return Object.assign({}, state, {
+        products: action.products
+      });
+
+    case FETCH_PRODUCTS_ERROR:
+      return Object.assign({}, state, {
+        error: action.error
+      });
+
     default:
       return state;
   }
 }
+
+
+export const getProducts = state => state.products;
+export const getProductsPending = state => state.pending;
+export const getProductsError = state => state.error;
