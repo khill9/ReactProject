4e3062f390abe6cbafbe6c66675bcc68658ee145
Kaylie Hill <kaylie.hill@regions.com>
2019-12-20 11:58:35 -0600

    cleanup

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { useSpring, animated } from 'react-spring'
 import './Card.css';
 
@@ -9,12 +8,6 @@ const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg)
 function Card(user) {
   const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
   return (
-    //<animated.div
-    //  class="card"
-     // onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-     // onMouseLeave={() => set({ xys: [0, 0, 1] })}
-     // style={{ transform: props.xys.interpolate(trans) }}
-   // />
    <animated.div class="animatedCard"
     onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
     onMouseLeave={() => set({ xys: [0, 0, 1] })}
